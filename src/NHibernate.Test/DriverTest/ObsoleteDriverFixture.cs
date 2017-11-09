1182e8720528d520dc89923ad19f76bb151bfee4
Nathan Brown <nathan@nkbrown.us>
2017-11-08 17:05:58 -0700

    NH-4008 - Enable separate drivers for tests.

---
+++ src/NHibernate.Test/DriverTest/ObsoleteDriverFixture.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Linq.Dynamic.Core;
+using NHibernate.Cfg;
+using NHibernate.Connection;
+using NHibernate.Dialect;
+using NHibernate.DomainModel;
+using NHibernate.Driver;
+using NUnit.Framework;
+
+namespace NHibernate.Test.DriverTest
+{
+	[TestFixture, Obsolete]
+	public class ObsoleteDriverFixture : TestCase
+	{
+		protected override IList Mappings => new [] {"Simple.hbm.xml"};
+
+		protected override bool AppliesTo(Dialect.Dialect dialect)
+		{
+			switch (dialect)
+			{
+				case FirebirdDialect _:
+				case MsSql2000Dialect _:
+				case MsSqlCeDialect _:
+				case MySQLDialect _:
+				case PostgreSQLDialect _:
+				case SQLiteDialect _:
+					return true;
+				default:
+					return false;
+			}
+		}
+
+		protected override void Configure(Configuration configuration)
+		{
+			base.Configure(configuration);
+			var dialect = NHibernate.Dialect.Dialect.GetDialect(configuration.Properties);
+			System.Type driver;
+			switch (dialect)
+			{
+				case FirebirdDialect _:
+					driver = typeof(FirebirdClientDriver);
+					break;
+				case MsSql2008Dialect _:
+					driver = typeof(Sql2008ClientDriver);
+					break;
+				case MsSql2000Dialect _:
+					driver = typeof(SqlClientDriver);
+					break;
+				case MsSqlCeDialect _:
+					driver = typeof(SqlServerCeDriver);
+					break;
+				case MySQLDialect _:
+					driver = typeof(MySqlDataDriver);
+					break;
+				case PostgreSQLDialect _:
+					driver = typeof(NpgsqlDriver);
+					break;
+				case SQLiteDialect _:
+					driver = typeof(SQLite20Driver);
+					break;
+				default:
+					return;
+			}
+			configuration.SetProperty(Cfg.Environment.ConnectionDriver, driver.FullName);
+		}
+
+		[Test]
+		public void GenerateObsoleteWarning()
+		{
+			using (var spy = new LogSpy(typeof(ConnectionProvider)))
+			{
+				using (BuildSessionFactory())
+				{
+					Assert.That(spy.GetWholeLog(), Does.Contain("Install the NHibernate.Driver."));
+				}
+			}
+		}
+
+		[Test]
+		public void CanUseObsoleteDriver()
+		{
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				var count = s.Query<Simple>().Count();
+				Assert.That(count, Is.Zero);
+				t.Commit();
+			}
+		}
+	}
+}
